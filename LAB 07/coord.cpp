5f66423c085822279db3d8c3ffcfe7d7a78532f8
Horst <horstjw.fang@gmail.com>
2025-11-10 21:14:16 -0500

    v1

--- LAB 07/coord.cpp
+++ LAB 07/coord.cpp
@@ -3,31 +3,48 @@
 void get_point(Point3D &pt){
     cout << "Enter ID: ";
     cin >> pt.id;
-    cout << endl << "Point ID: " << pt.id << endl;
+    cout << "Point ID: " << pt.id << endl << endl;
 
     cout << "Enter X coordinate: ";
     cin >> pt.x;
-    cout << endl << "X Coordinate: " << pt.x << endl;
+    cout << "X Coordinate: " << pt.x << endl << endl;
 
     cout << "Enter Y coordinate: ";
     cin >> pt.y;
-    cout << endl << "Y Coordinate: " << pt.y << endl;
+    cout << "Y Coordinate: " << pt.y << endl << endl;
 
     cout << "Enter Z coordinate: ";
     cin >> pt.z;
-    cout << endl << "Z Coordinate: " << pt.z << endl;
+    cout << "Z Coordinate: " << pt.z << endl << endl;
 
     cout << "Enter accuracy order (A/B/C): ";
     cin >> pt.order;
-    cout << endl << "Order: " << pt.order << endl;
+    cout << "Order: " << pt.order << endl << endl;
     
     return;
 }
 
 void print_point(const Point3D &pt) {
-    cout << "Point ID: "      << setw(10) << pt.id << endl;
+    cout << "Point ID:     "  << setw(10) << pt.id << endl;
     cout << "X Coordinate: "  << setw(10) << pt.x << endl;
     cout << "Y Coordinate: "  << setw(10) << pt.y << endl;
     cout << "Z Coordinate: "  << setw(10) << pt.z << endl;
-    cout << "Order: "         << setw(10) << pt.order << endl;
+    cout << "Order:        "  << setw(10) << pt.order << endl << endl;
+}
+
+double spatial_dist(const Point3D &pt1, const Point3D &pt2){
+    return sqrt((pt1.x + pt2.x) * (pt1.x + pt2.x) +(pt1.y + pt2.y) * (pt1.y + pt2.y) + (pt1.z + pt2.z) * (pt1.z + pt2.z));
+}
+
+double plani_dist(const Point3D &pt1, const Point3D &pt2){
+    return sqrt((pt1.x + pt2.x) * (pt1.x + pt2.x) +(pt1.y + pt2.y) * (pt1.y + pt2.y));
+}
+
+double height_diff(const Point3D &pt1, const Point3D &pt2){
+    return pt2.z - pt1.z;
+}
+
+double azimuth(const Point3D &pt1, const Point3D &pt2){
+    double rad = atan2((pt2.y - pt1.y), (pt2.x - pt1.x));
+    return rad / M_PI * 180;
 }
--- LAB 07/gps.cpp
+++ LAB 07/gps.cpp
@@ -8,5 +8,9 @@ int main(){
     print_point(point1);
     print_point(point2);
 
+    cout << "Spatial Distance: " << spatial_dist(point1, point2) << " units" << endl;
+    cout << "Planimetric Distance: " << plani_dist(point1, point2) << " units" << endl;
+    cout << "Height Difference: " << height_diff(point1, point2) << " units" << endl;
+    cout << "Heading: " << azimuth(point1, point2) << " degrees" << endl;
     return 0;
 }
